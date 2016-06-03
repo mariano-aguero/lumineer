1d91088a7c8805d2ebb759f25496994c14ede828
Vince Kronlein <vkronlein@icloud.com>
2016-06-02 17:31:00 -0700

    added helper file
    Signed-off-by: Vince Kronlein <vkronlein@icloud.com>

---
+++ src/helpers.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Lumineer role & 
+ * permission management solution for Lumen.
+ *
+ * @author Vince Kronlein <vince@19peaches.com>
+ * @license https://github.com/19peaches/lumineer/blob/master/LICENSE
+ * @copyright 19 Peaches, LLC. All Rights Reserved.
+ */
+
+if ( ! function_exists('config_path'))
+{
+    /**
+     * Get the configuration path.
+     *
+     * @param  string $path
+     * @return string
+     */
+    function config_path($path = '')
+    {
+        return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+    }
+}
